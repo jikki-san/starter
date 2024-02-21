4e3d00e1112b1f99331be759140e000133efb350
Jackson Eckert <jaxon.eckert@gmail.com>
2024-02-20 23:42:46 -0600

    Add full server startup and placeholder typedefs

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,27 @@
-// TODO: Basic server implementation
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { addMocksToSchema } from "@graphql-tools/mock";
 import { makeExecutableSchema } from "@graphql-tools/schema";
-
 import { typeDefs } from "./schema";
 
+const mocks = {
+  Query: () => ({
+    things: () => [...new Array(4)],
+  }),
+  Thing: () => ({
+    amabob: () => "amajig",
+  }),
+};
+
 async function startApolloServer() {
-  const server = new ApolloServer({ typeDefs });
+  const server = new ApolloServer({
+    schema: addMocksToSchema({
+      schema: makeExecutableSchema({ typeDefs }),
+      mocks,
+    }),
+  });
   const { url } = await startStandaloneServer(server);
-  console.log(`
-    🚀  Server is running!
-    📭  Query at ${url}
-  `);
+  console.log(`Server is running! Query at ${url}`);
 }
 
 startApolloServer();
--- server/src/schema.ts
+++ server/src/schema.ts
@@ -3,7 +3,13 @@ import gql from "graphql-tag";
 export const typeDefs = gql`
   type Query {
     # Query definitions go here
+    # Replace with your own Query definitions
+    things: [Thing!]!
   }
 
   # Schema definitions go here
+  # Replace with your own schema config
+  type Thing {
+    amabob: String!
+  }
 `;
