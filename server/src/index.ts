d964eb0a623ce6747e41d4f74081a2744b9163c2
Jackson Eckert <jaxon.eckert@gmail.com>
2024-02-20 21:50:52 -0600

    Add Apollo Server base

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,18 @@
 // TODO: Basic server implementation
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { addMocksToSchema } from "@graphql-tools/mock";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+
+import { typeDefs } from "./schema";
+
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs });
+  const { url } = await startStandaloneServer(server);
+  console.log(`
+    🚀  Server is running!
+    📭  Query at ${url}
+  `);
+}
+
+startApolloServer();
---
+++ server/src/schema.ts
@@ -0,0 +1,9 @@
+import gql from "graphql-tag";
+
+export const typeDefs = gql`
+  type Query {
+    # Query definitions go here
+  }
+
+  # Schema definitions go here
+`;
